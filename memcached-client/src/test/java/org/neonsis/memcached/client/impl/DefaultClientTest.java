216df4c444198b447097dd3f258487b55f1dbf96
Andrey Vinel <vinel.work03@gmail.com>
2020-08-01 19:49:25 +0300

    add tests for client

--- memcached-client/src/test/java/org/neonsis/memcached/client/impl/DefaultClientTest.java
+++ memcached-client/src/test/java/org/neonsis/memcached/client/impl/DefaultClientTest.java
@@ -15,6 +15,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -66,6 +68,108 @@ public class DefaultClientTest {
         assertEquals(Status.GOTTEN, response.getStatus());
         verify(requestConverter).writeRequest(out, request);
         verify(responseConverter).readResponse(in);
+    }
+
+    @Test
+    public void putSimple() throws IOException {
+        byte[] array = {1, 2, 3};
+        String key = "key";
+        Object value = "value";
+
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.ADDED));
+        when(serializer.toByteArray(value)).thenReturn(array);
+
+        Status status = client.put(key, value);
+
+        assertEquals(Status.ADDED, status);
+        verify(serializer).toByteArray(value);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.PUT, key, null, array)));
+    }
+
+    @Test
+    public void putFull() throws IOException {
+        byte[] array = {1, 2, 3};
+        String key = "key";
+        Object value = "value";
+
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.REPLACED));
+        when(serializer.toByteArray(value)).thenReturn(array);
+
+        Status status = client.put(key, value, 1, TimeUnit.MILLISECONDS);
+
+        assertEquals(Status.REPLACED, status);
+        verify(serializer).toByteArray(value);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.PUT, key, 1L, array)));
+    }
+
+    @Test
+    public void putFullInvalidTTL() throws IOException {
+        byte[] array = {1, 2, 3};
+        String key = "key";
+        Object value = "value";
+
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.REPLACED));
+        when(serializer.toByteArray(value)).thenReturn(array);
+
+        Status status = client.put(key, value, 1, null);
+
+        assertEquals(Status.REPLACED, status);
+        verify(serializer).toByteArray(value);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.PUT, key, null, array)));
+    }
+
+    @Test
+    public void getSuccess() throws IOException {
+        byte[] array = {1, 2, 3};
+        String key = "key";
+        Object value = "value";
+
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.GOTTEN, array));
+        when(serializer.fromByteArray(array)).thenReturn(value);
+
+        String result = client.get(key);
+
+        assertEquals(value, result);
+        verify(serializer).fromByteArray(array);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.GET, key)));
+    }
+
+    @Test
+    public void removeSuccess() throws IOException {
+        String key = "key";
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.REMOVED));
+
+        Status remove = client.remove(key);
+
+        assertEquals(Status.REMOVED, remove);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.REMOVE, key)));
+
+    }
+
+    @Test
+    public void clearSuccess() throws IOException {
+        when(responseConverter.readResponse(in)).thenReturn(new Response(Status.CLEARED));
+
+        Status remove = client.clear();
+
+        assertEquals(Status.CLEARED, remove);
+        verify(requestConverter).writeRequest(same(out), equalTo(new Request(Command.CLEAR)));
+    }
+
+    @Test
+    public void closeSuccess() throws IOException {
+        client.close();
+
+        verify(socket).close();
+    }
+
+    private Request equalTo(Request request) {
+        return argThat(request1 ->
+                Objects.equals(request.getCommand(), request1.getCommand()) &&
+                        Objects.equals(request.getKey(), request1.getKey()) &&
+                        Objects.equals(request.getTtl(), request1.getTtl()) &&
+                        Objects.equals(request.getData(), request1.getData())
+        );
     }
 
 }
